d908e7a44d8e8cfa74b4348baa389e7ddd7d3e91
Vitaly <39009460+vitalybelo@users.noreply.github.com>
2022-12-26 12:28:16 +0300

    check_out

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
-#include <iostream>
 #include "solutions.h"
 
 int main() {
-
     // Реализованы HTTP запросы: GET, POST, PUT, PATCH, DELETE
     solution_1();
 
@@ -11,7 +9,6 @@ int main() {
 
     // Реализованы запросы GET (с передачей параметров) и POST (с передачей параметров)
     solution_3();
-
 
     return 0;
 }
